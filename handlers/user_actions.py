88f5439131f92f0967371db13603b16db5fefded
AndrewLomonosov <lomonosov.kfu@gmail.com>
2023-06-22 18:43:32 +0200

    Added registration message

--- handlers/user_actions.py
+++ handlers/user_actions.py
@@ -4,11 +4,18 @@ from aiogram.dispatcher.filters import Text
 from Keyboards import Buttons
 from handlers.HandlerOpenAI import ChatGPTstates
 import MessageBox
+from config import BOT_OWNERS
+from dispatcher import bot
 
 
 # Define the start command handler
 async def start_handler(message: types.Message):
     await message.answer(MessageBox.START_MESSAGE, reply_markup=Buttons.kbMenu, parse_mode='html')
+    user_id = message.from_user.id
+    user_name = message.from_user.full_name
+    await bot.send_message(BOT_OWNERS[0], f'New user have connected: {user_name}\n'
+                                          f'His ID: {user_id}')
+
     await ChatGPTstates.dialog_chatgpt.set()
 
 # Define the cancel command handler
